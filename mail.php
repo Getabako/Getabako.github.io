42a9834f71533ce536aeda35f8891d31924a1aed
Getabako <34262504+Getabako@users.noreply.github.com>
2020-08-09 14:41:35 +0900

    ファイル設置

---
+++ mail.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; "charset="utf-8">
+	<title>WonderWorks</title>
+</head>
+<body>
+<form class="mailForm" action="mailsend.php" method="post">
+     <p>お気軽にお問い合わせください。</p>
+     <dl>
+       <dt>お名前</dt>
+       <dd>
+         <input name="name" type="text" required="required" id="name">
+       </dd>
+       <dt>メールアドレス</dt>
+       <dd>
+         <input name="email" type="email" required="required" id="email">
+       </dd>
+       <dt>お問い合わせ内容</dt>
+       <dd>
+         <textarea name="content" required="required" id="content"></textarea>
+       </dd>
+       <input type="hidden" id="token" name="token" value="1234567" />
+     </dl>
+     <p id="submit_button_cover">
+       <input name="submit_buton" type="submit" id="submit_button" value="送信">
+     </p>
+</form>
+</body>
+</html>
